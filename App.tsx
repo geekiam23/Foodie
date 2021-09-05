90d66ebce93c26f14cd3ef154bea94f3db1b7c34
Will Larry <will23larry@gmail.com>
2021-09-05 16:26:36 -0400

    Bug fixes.

--- App.tsx
+++ App.tsx
@@ -1,44 +1,42 @@
 import 'react-native-gesture-handler';
-import React, {FC} from 'react';
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from 'screens/HomeScreen';
+import RecipeScreen from 'screens/RecipeScreen';
 
 import HeaderIcons from 'components/common/HeaderIcons';
 
 const Stack = createStackNavigator();
 
-const App: FC = () => {
-  const HomeStack = () => (
-    <>
-      <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{
-          title: 'My Recipes',
-          headerStyle: {
-            backgroundColor: '#092235',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 20,
-            paddingBottom: 5,
-          },
-          headerRight: () => <HeaderIcons />,
-        }}
-      />
-      <Stack.Screen
-        name="RecipeScreen"
-        component={RecipeScreen}
-        options={({route}) => ({title: route?.params?.recipe.title})}
-      />
-  );
-};
+const HomeStack = () => (
+  <>
+    <Stack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{
+        title: 'My Recipes',
+        headerStyle: {
+          backgroundColor: '#092235',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 20,
+          paddingBottom: 5,
+        },
+        headerRight: () => <HeaderIcons />,
+      }}
+    />
+    <Stack.Screen
+      name="RecipeScreen"
+      component={RecipeScreen}
+      options={({route}) => ({title: route?.params?.recipe.title})}
+    />
+  </>
+);
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return <Stack.Navigator>{HomeStack()}</Stack.Navigator>;
 };
 
